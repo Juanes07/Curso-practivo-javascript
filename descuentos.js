67ec7c033a2ec83adcc536ba9bdea9b55ae473a9
Juanes07 <juanesdm3@gmail.com>
2021-12-20 15:20:39 -0500

    se agrega el archivo descuento.js descuentos.html, se modifica tambien el archivo README

---
+++ descuentos.js
@@ -0,0 +1,33 @@
+const precioOriginal = 120;
+const descuento = 18;
+
+
+function calcularPrecioConDescuento(precio,descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento / 100);
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    const InputDiscount = document.getElementById("InputDiscount");
+    const discountValue = InputDiscount.value;
+    const precioConDescuento = calcularPrecioConDescuento(priceValue,discountValue);
+    const resultP = document.getElementById("ResultP");
+    resultP.innerHTML ="El precio con descuento es: $" + precioConDescuento;
+}
+
+
+
+
+
+
+
+
+// console.log({
+//     precioOriginal,
+//     descuento,
+//     porcentajePrecioConDescuento,
+//     precioConDescuento,
+// });
