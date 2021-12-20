a2b7ec6dc06d08a4f7ed6220baa45a3c47e280ac
Juanes07 <juanesdm3@gmail.com>
2021-12-20 15:56:42 -0500

    trackeo de nuevos archivos

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,4 @@
-const precioOriginal = 120;
-const descuento = 18;
-
+let descuento;
 
 function calcularPrecioConDescuento(precio,descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
@@ -9,13 +7,37 @@ function calcularPrecioConDescuento(precio,descuento){
 }
 
 function onClickButtonPriceDiscount(){
+    const cupones =["code10","code20","code30"];
+
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
-    const InputDiscount = document.getElementById("InputDiscount");
-    const discountValue = InputDiscount.value;
-    const precioConDescuento = calcularPrecioConDescuento(priceValue,discountValue);
+
+    const inputCoupon  = document.getElementById("InputCoupon");
+    const couponValue  = inputCoupon.value;
+    let descuento;
+
+    switch(couponValue){
+        case "code10":
+            descuento =10;
+            break;
+        case "code20":
+            descuento = 20;
+            break;
+        case "code30":
+            descuento =30;
+            break;
+        default:
+            descuento = 0;
+            break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue,couponValue);
     const resultP = document.getElementById("ResultP");
+
     resultP.innerHTML ="El precio con descuento es: $" + precioConDescuento;
+    
+    
+
 }
 
 
